c954fe44d007f4e53a7681724d5dc90b34b7989e
myst-6 <mborishall@gmail.com>
2022-03-18 19:45:30 +0000

    react-routing to fix static file path issues

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/">
+                    <App />
+                </Route>
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
